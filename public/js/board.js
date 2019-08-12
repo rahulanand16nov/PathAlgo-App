b0944c2524c1c542dc82a35d8bd3903d780f1e5c
Rahul <rahulanand16nov@gmail.com>
2019-08-12 16:54:38 +0530

    Added moveable start and end points

--- public/js/board.js
+++ public/js/board.js
@@ -3,7 +3,19 @@ function Node(id, status) {
     this.status = status;
   }
 
+// Required Global Data
 var boardArray = [];
+var height;
+var width;
+
+function initialize(h,w){
+  height = h;
+  width = w;
+  window.startId = "row:0-col:0";
+  window.endId = `row:${height-1}-col:${width-1}`;
+  createGrid(height,width);
+  eventListeners(height,width);
+}
 
 // Function that generate the grid
 createGrid = function(height,width){
@@ -14,7 +26,13 @@ createGrid = function(height,width){
     this.nodes={};
     for (let c = 0; c < width; c++) {
       let newNodeId = `row:${r}-col:${c}`, newNodeClass, newNode;
-      newNodeClass = "unvisited";
+      if(r===0 && c===0){
+        newNodeClass = "start"
+      } else if (r===height-1 && c === width-1){
+        newNodeClass = "end";
+      } else {
+        newNodeClass = "unvisited";
+      }
       newNode = new Node(newNodeId, newNodeClass);
       currentArrayRow.push(newNode);
       currentHTMLRow += `<td id="${newNodeId}" class="${newNodeClass}"></td>`;
@@ -27,15 +45,76 @@ createGrid = function(height,width){
   board.innerHTML = tableHTML;
 }
 
+// Functions adds event listeners to the nodes
 eventListeners = function(height,width){
     for(let r=0;r<height;r++){
         for(let c=0;c<width;c++){
             let currentId = `row:${r}-col:${c}`;
             let currentElement = document.getElementById(currentId);
-            currentElement.addEventListener('mousedown',()=>{
+
+
+
+            let ctrldown = false;
+            let sdown = false;
+            let edown = false;
+
+            currentElement.addEventListener('mousedown',(e)=>{
+
+              if(currentElement.className === "unvisited" && sdown === true){
+                document.getElementById(startId).className = "unvisited";
+                startId = currentId;
+                currentElement.className = "start";
+              }
+
+              if(currentElement.className === "unvisited" && edown === true){
+                console.log(endId);
+                document.getElementById(endId).className = "unvisited";
+                endId = currentId;
+                currentElement.className = "end";
+              } 
+
+              if(currentId!==startId && currentId !== endId && currentElement.className!=="brick" && e.which===1 && sdown===false){
                 currentElement.className="brick";
-                // For debugging purposes:
-                console.log(`Added a brick to ${boardArray[r][c].id}`)
+                //console.log(`Added a brick to ${boardArray[r][c].id}`);
+              }
+
+              if(currentElement.className === "brick" && ctrldown === true){
+                currentElement.className = "unvisited";
+                //console.log(`Removed brick from ${boardArray[r][c].id}`)
+              }
+
+            });
+
+            $(document).on({
+              keydown: (e)=>{
+                e.preventDefault();
+                // If ctrl key is pressed
+                if(e.which===17){
+                  ctrldown=true;
+                }
+                //if 's'is pressed
+                if(e.which===83){
+                  sdown = true;
+                }
+
+                if(e.which===69){
+                  edown = true;
+                }
+              },
+              keyup: (e)=>{
+              // If ctrl key is pressed
+              if(e.which===17){
+                ctrldown=false;
+              }
+              //if 's'is pressed
+              if(e.which===83){
+                sdown = false;
+              }
+
+              if(e.which === 69){
+                edown = false;
+              }
+            }
             });
         }
     }
